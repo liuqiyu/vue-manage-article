acc886e4e33cda9128ce0255fd131e7911ed0217
liuqiyu <1064839051@qq.com>
2019-10-09 15:29:31 +0800

    perf: 优化validate 检测手机和电话

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,6 +1,44 @@
 /**
- * Created by PanJiaChen on 16/11/18.
+ * Created by Kaway on 2019/19/9.
  */
+
+import pattern from './pattern'
+
+/**
+ * 电话号码
+ * @param {object} rule
+ * @param {string} value
+ * @param {function} callback
+ */
+export const validTel = (rule, value, callback) => {
+  if (value) {
+    if (pattern.tel.test(value)) {
+      callback()
+    } else {
+      return callback(new Error('输入的电话号码不合法'))
+    }
+  } else {
+    return callback(new Error('请输入电话号码'))
+  }
+}
+
+/**
+ * 手机号码
+ * @param {object} rule
+ * @param {string} value
+ * @param {function} callback
+ */
+export const phone = (rule, value, callback) => {
+  if (value) {
+    if (pattern.phone.test(value)) {
+      callback()
+    } else {
+      return callback(new Error('手机号码格式不正确'))
+    }
+  } else {
+    return callback(new Error('手机号码不能为空'))
+  }
+}
 
 /**
  * @param {string} path
