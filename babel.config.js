250eebb9ee2a1791390a324f315a976e1b61fb08
liuqiyu <1064839051@qq.com>
2019-10-12 17:13:24 +0800

    chore: 生产环境取消console.log

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,10 @@
+let plugins = []
+if (process.env.NODE_ENV === 'production') {
+  plugins.push('transform-remove-console')
+}
+
 module.exports = {
+  plugins: plugins,
   presets: [
     ['@vue/app', {
       useBuiltIns: 'entry'
