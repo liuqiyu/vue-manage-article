9ad6f0d49d101f49264f898fa179a04c88b3cf9e
liuqiyu <1064839051@qq.com>
2019-11-04 21:01:11 +0800

    feat: feated clipboard page

--- src/views/tabs/index.vue
+++ src/views/tabs/index.vue
@@ -1,19 +1,5 @@
 <template>
   <yus-content-page>
-    <el-table :data="tableData"
-              style="width: 100%">
-      <el-table-column prop="date"
-                       label="日期"
-                       width="180">
-      </el-table-column>
-      <el-table-column prop="name"
-                       label="姓名"
-                       width="180">
-      </el-table-column>
-      <el-table-column prop="address"
-                       label="地址">
-      </el-table-column>
-    </el-table>
     <yus-content-row>
       <yus-content-cell :title="'使用Keep-alive实现缓存'">
         <el-tabs type="border-card"
