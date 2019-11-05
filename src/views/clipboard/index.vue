1aba8409b9b0ee4d668d228e293ad9bb0ffa7adc
liuqiyu <1064839051@qq.com>
2019-11-05 18:10:12 +0800

    test: lazyMan

--- src/views/clipboard/index.vue
+++ src/views/clipboard/index.vue
@@ -1,3 +1,10 @@
+<!--
+ * @Descripttion:
+ * @Author: liuqiyu
+ * @Date: 2019-11-05 09:24:56
+ * @LastEditors: liuqiyu
+ * @LastEditTime: 2019-11-05 18:04:20
+ -->
 <template>
   <yus-content-page>
     <el-tabs class="clipboard"
@@ -31,6 +38,64 @@ export default {
       input: '请单击复制',
       input2: '请双击复制'
     }
+  },
+  mounted () {
+    function $ (element) {
+      return new Base(element)
+    }
+
+    function Base (element) {
+      this.taskList = []
+
+      this.init = function (func) {
+
+      }
+      this.firstSleep = function (time) {
+        var task = function () {
+          return setTimeout(() => {
+            console.log('第一休息' + time + '秒')
+          }, time * 1000)
+        }
+
+        this.taskList.push(task)
+        console.log('firstSleep')
+        return this
+      }
+
+      this.sleep = function (time) {
+        var task = function () {
+          return setTimeout(() => {
+            console.log('第一休息' + time + '秒')
+          }, time * 1000)
+        }
+
+        this.taskList.push(task)
+
+        console.log('sleep')
+
+        return this
+      }
+
+      this.say = function (name) {
+        var task = function () {
+          return console.log('say' + name)
+        }
+        this.taskList.push(task)
+        console.log('say')
+        return this
+      }
+
+      this.eat = function (name) {
+        var task = function () {
+          return console.log('eat' + name)
+        }
+        this.taskList.push(task)
+        console.log('eat')
+        return this
+      }
+    }
+
+    $(123).sleep(5).say('张三').firstSleep(5).eat('排骨')
   }
 }
 </script>
